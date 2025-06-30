eebb411b6e08666cc368359f556d8f6c2094a522
Aryaman Patra <aryamanprivate26@gmail.com>
2025-06-30 11:22:50 +0530

    feat: app log file untracked, log4j working

--- Backend/farmersmarkethub/src/main/java/com/group1/farmersmarkethub/model/Payment.java
+++ Backend/farmersmarkethub/src/main/java/com/group1/farmersmarkethub/model/Payment.java
@@ -1,89 +1,41 @@
 package com.group1.farmersmarkethub.model;
 
-import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
-@Entity
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity(name="payments")
+@Table
+@Data
 public class Payment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name="payment_method")
     private String paymentMethod; // e.g., "Credit Card", "UPI", etc.
+
+    @Column(name="transaction_id")
     private String transactionId;
-    private Double amount;
+
+    private double amount;
+
+    @Column(name="payment_time")
     private LocalDateTime paymentTime;
+    
     private String status; // e.g., "PAID", "FAILED", "PENDING"
 
     @OneToOne
     @JoinColumn(name = "order_id")
     private Order order;
-    public Payment() {
-    }
-
-    public Payment(String paymentMethod, String transactionId, Double amount, LocalDateTime paymentTime, String status, Order order) {
-        this.paymentMethod = paymentMethod;
-        this.transactionId = transactionId;
-        this.amount = amount;
-        this.paymentTime = paymentTime;
-        this.status = status;
-        this.order = order;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getPaymentMethod() {
-        return paymentMethod;
-    }
-
-    public void setPaymentMethod(String paymentMethod) {
-        this.paymentMethod = paymentMethod;
-    }
-
-    public String getTransactionId() {
-        return transactionId;
-    }
-
-    public void setTransactionId(String transactionId) {
-        this.transactionId = transactionId;
-    }
-
-    public Double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(Double amount) {
-        this.amount = amount;
-    }
-
-    public LocalDateTime getPaymentTime() {
-        return paymentTime;
-    }
-
-    public void setPaymentTime(LocalDateTime paymentTime) {
-        this.paymentTime = paymentTime;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public Order getOrder() {
-        return order;
-    }
-
-    public void setOrder(Order order) {
-        this.order = order;
-    }
+    
 }
